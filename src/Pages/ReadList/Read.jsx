319dc34958b8807c7a3157b06c0393cff0462e6e
MD Saifur Rahaman <asifforizy@gmail.com>
2026-02-10 17:47:21 +0600

    sort funcyionality added

--- src/Pages/ReadList/Read.jsx
+++ src/Pages/ReadList/Read.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Read = ({ book }) => {
     return (
-        <div className='border border-gray-300 rounded-lg p-4 m-4 flex justify-between items-center'>
-            <div className='flex gap-10'>
+        <div className='border border-gray-300 rounded-lg p-4 m-4 flex justify-between items-center'> 
+            <div className='flex gap-10 items-center'>
                 <div>
                     <img src={book.image} alt={book.bookName} className="w-18 h-auto " />
                 </div>
@@ -11,12 +11,15 @@ const Read = ({ book }) => {
                 <div>
                     <h2 className='text-2xl font-semibold mb-2'>{book.bookName}</h2>
                     <p className='text-gray-500 mb-2'>Author: {book.author}</p>
-                    <p className='text-gray-500 mb-2'>Category: {book.category}</p>
+                    <div className='flex gap-5 text-gray-500 mb-2'>
+                        <p>pages : {book.totalPages}</p>
+                        <p>ratings : {book.rating}</p>
+                    </div>
                 </div>
             </div>
 
             <div>
-                <h1 className='text-red-600 text-3xl mr-10'>x</h1>
+                <h1 className='text-red-600 text-3xl lg:mr-10'>x</h1>
             </div>
         </div>
     );
--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
@@ -8,20 +8,44 @@ import Read from './Read';
 const ReadList = () => {
     const data = useLoaderData() || [];
 
-    const [readList , setReadList] = React.useState([]);
+    const [readList, setReadList] = React.useState([]);
+    const [sort, setSort] = useState('')
 
-    useEffect(()=> {
+    useEffect(() => {
         const storedBookData = getStoredBooks();
         const convertedData = storedBookData.map(id => parseInt(id));
         const readBooks = data.filter(book => convertedData.includes(book.bookId));
         setReadList(readBooks);
+    }, [])
+
+     const handleSort= (type)=> {
+
+            setSort(type);
+            if(type === 'pages'){
+                const sortByPages = [...readList].sort((a,b)=> a.totalPages - b.totalPages);
+                setReadList(sortByPages);
+            }
+            if(type === 'ratings'){
+                const sortByRatings = [...readList].sort((a,b)=> a.rating - b.rating);
+                setReadList(sortByRatings);
+
+            }
 
 
-    },[])
+        }
 
     return (
         <div>
-            <h1 className='text-3xl font-bold text-center my-10'>Books</h1>
+            <div className='flex justify-between items-center mx-5'>
+                <h1 className='text-3xl font-bold  my-10'>Books</h1>
+                <div className="dropdown  ">
+                    <div tabIndex={0} role="button" className="btn m-1">sort by : {sort ? sort : ''}</div>
+                    <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a onClick={()=>handleSort('pages')}>pages</a></li>
+                        <li><a onClick={()=>handleSort('ratings')}>ratings</a></li>
+                    </ul>
+                </div>
+            </div>
 
             <Tabs>
                 <TabList>
@@ -31,7 +55,7 @@ const ReadList = () => {
 
                 <TabPanel>
                     {
-                        readList.map(book=> <Read key={book.bookId} book={book}></Read>)
+                        readList.map(book => <Read key={book.bookId} book={book}></Read>)
                     }
                 </TabPanel>
                 <TabPanel>
